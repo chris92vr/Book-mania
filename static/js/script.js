3cdc1f1b31b331e03c3a586d75ce3d90d9dc9ed2
chris92vr <garofolichristian@gmail.com>
2021-01-14 00:54:41 +0000

    Added comment, cleaned code

--- static/js/script.js
+++ static/js/script.js
@@ -1,21 +1,37 @@
-$(document).ready(function(){
+// INITIALIZE MATERIALIZE
+
+/** 
+ * Function that initializes materialize side navbar, accordion collapsible,
+ * select menu and tabs
+ **/
+$(document).ready(function() {
     $('.sidenav').sidenav();
-    $('.carousel').carousel({indicators: true});
+    $('.carousel').carousel({
+        indicators: true
+    });
     $('select').formSelect();
     $('.modal').modal();
-  });
+});
 
-  var btn = $('#button');
+// BACK TO TOP BUTTON
+
+/**
+ * Function implements smooth scrolling back to top after clicking the button
+ */
+
+var btn = $('#button');
 
 $(window).scroll(function() {
-  if ($(window).scrollTop() > 300) {
-    btn.addClass('show');
-  } else {
-    btn.removeClass('show');
-  }
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
 });
 
 btn.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: 0
+    }, '300');
 });
