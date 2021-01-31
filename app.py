e296ee0a8ab053f9341a5c0a61c5afae364567d4
chris92vr <garofolichristian@gmail.com>
2021-01-31 23:20:09 +0000

    Set button back to the top, capitalized form registration and login label

--- app.py
+++ app.py
@@ -185,7 +185,7 @@ def login():
         if existing_user:
             # ensure hashed password matches user input
             if check_password_hash(
-                    existing_user["password"], form.password.data):
+                    existing_user["password"], form.Password.data):
                     session["user"] = form.Username.data.lower()
                     flash("Welcome, {}".format(
                           request.form.get("username")))
